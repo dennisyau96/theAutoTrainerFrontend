1cc9bb63c5cfcbf22de16e20ea08b0c745127b3e
Dennis Yau <>
2025-05-19 20:02:07 -0700

    final fix

--- src/component/Program.jsx
+++ src/component/Program.jsx
@@ -91,7 +91,7 @@ const Program = () => {
             </div>
           </div>
 
-          <div className=" mx-10">
+          <div className=" mx-5">
             <div
               className="p-4 text-2xl text-white font-monospace flex flex-wrap text-center"
               id="PTIP"
@@ -127,15 +127,16 @@ const Program = () => {
               do best.
             </div>
 
-            <div className="Ptip m-4" id="Ptip">
+            <div className="Ptip p-2" id="Ptip">
               <h3>Paul’s tips of the day</h3>
-              <div className="m-3 italic">
+              <div className="m-2 italic text-beige-100">
                 The first 10 words you say are more important than your next
                 1,000. Please don't ask a customer if they have been helped when
                 you have seen then stand in your showroom for 5 minutes by
                 themselves.
               </div>
-              <div className="m-3 font-bold">
+              <hr />
+              <div className="m-2 font-bold text-yellow-100">
                 Never make the mistake of prejudging a potential customer just
                 because they are wearing casual clothing in your dealership.
                 Treat them always like you wanted to be treated and the results
@@ -143,7 +144,8 @@ const Program = () => {
                 all about training and motivating people that are at a
                 crossroads in their career.
               </div>{" "}
-              <div className="m-3 italic ">
+              <hr />
+              <div className="m-2 italic text-slate-300">
                 No experience or some experience doesn't really matter. We want
                 people to embrace the concepts of building a successful platform
                 through our mentoring and coaching program. More often than not
