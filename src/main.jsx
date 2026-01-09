9ddf2a7951ea6b956ff2533e4b3cdaf768a10d81
Dennis Yau <>
2026-01-09 15:01:42 -0800

    del tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import "./animation.css";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
